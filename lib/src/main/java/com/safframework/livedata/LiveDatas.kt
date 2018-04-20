48d515aa3b28c6698f57d5076e6d8cdcd43525d7
fengzhizi715 <fengzhizi715@126.com>
2018-04-20 11:05:00 +0800

    update LiveDatas

---
+++ lib/src/main/java/com/safframework/livedata/LiveDatas.kt
@@ -0,0 +1,10 @@
+package com.safframework.livedata
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.livedata.LiveDatas.java
+ * @author: Tony Shen
+ * @date: 2018-04-20 11:04
+ * @version V1.0 <描述当前版本功能>
+ */
